55513c87f4d366db478681e7ba203602f5613682
Aarti Khunti <139998760+AartiKhunti@users.noreply.github.com>
2024-01-26 11:17:50 +0000

    file upload

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+get_header(); 
+
+while (have_posts()) :
+    the_post();
+?>
+    <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+    </div>
+<?php endwhile;
+
+get_footer(); 
+?>
